c2970cc8da20076070eb19d11c7d05b7952ecc67
juan00juan0 <jherrera2343@gmail.com>
2024-02-25 11:45:56 -0600

    added random coordinate data

--- Detector-Parser/json_parsing.py
+++ Detector-Parser/json_parsing.py
@@ -1,15 +1,28 @@
 # This script converts data into JSON-compatible strings
 import json
 
-def createStringJSON(data: list) -> str:
-    json_data = {
-        "user_id": 1,
-        "detector_id":1,
-        "temperature": float(data[0]),
-        "pressure": float(data[1]),
-        "altitude": float(data[2])
-    }
-    
-    json_string = json.dumps(json_data)
-    
-    return json_string
+def createStringJSON(data: list, data_type: str) -> str:
+    if data_type == "climate":
+        json_data = {
+            "user_id": 1,
+            "detector_id":1,
+            "temperature": float(data[0]),
+            "pressure": float(data[1]),
+            "altitude": float(data[2])
+        }
+        
+        json_string = json.dumps(json_data)
+        
+        return json_string
+    else:
+        json_data = {
+            "user_id": 1,
+            "detector_id":1,
+            "longitude": float(data[0]),
+            "latitude": float(data[1])
+        }
+        
+        json_string = json.dumps(json_data)
+        
+        return json_string
+
--- Detector-Parser/main.py
+++ Detector-Parser/main.py
@@ -1,6 +1,7 @@
 import data_collection as dc
 import json_requests as jr
 import json_parsing as jp
+import random
 # dumps -> serialize
 # loads -> deserialize
 
@@ -14,11 +15,31 @@ while(1):
             data_list = dc.readFromSerial()
             
             if dc.validSensorData(data_list):
-                # Create JSON string
-                bmp_json = jp.createStringJSON(data_list)
+                # Create JSON string from bmp data
+                bmp_json = jp.createStringJSON(data_list, "climate")
                 
-                # Send JSON string
+                # Send JSON string containing bmp data
                 # jr.sendDataJSON(bmp_json)
+                print(bmp_json)
+                
+                # Create random coordinate data
+                latitude = random.uniform(-70.0, 70.0)
+                longitude = random.uniform(-180.0, 190.0)
+                
+                latitude = "{:.2f}".format(latitude)
+                longitude = "{:.2f}".format(longitude)
+                
+                coord_list = [longitude, latitude]
+                
+                # Create JSON string from coordinate "data"
+                coord_json = jp.createStringJSON(coord_list, "coordinate")
+                
+                # Send JSON string containing coordinate data
+                # jr.sendDataJSON(coord_json)
+                print(coord_json)
+                
+                
+                
                 
     except KeyboardInterrupt:
         dc.serialTerminate()
